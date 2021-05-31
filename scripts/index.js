080d125abf16df0b982be521a6c245286c6db760
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-05-31 22:46:03 +0530

    basic-js

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const API_KEY = "at_DyfVD6rtsipdTDP7cipl1wmoGyvcT&ipAddress=8.8.8.8";
+const API_LINK = 'https://geo.ipify.org/api/'
+
+let current_ip = document.getElementById('ip-address');
+let current_location = document.getElementById('location');
+let current_timezone = document.getElementById('timezone');
+let current_isp = document.getElementById('isp');
+
+const entered_ip = document.getElementById('ip-address');
+const submit_btn = document.getElementById('submit-btn');
